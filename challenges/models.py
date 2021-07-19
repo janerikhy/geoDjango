406560adc8595f372e7eb0b6be2695f93f485ea7
Jan-Erik Hygen <j-e.hygen@hotmail.com>
2021-07-19 14:07:18 +0200

    Created first draft of a challange model

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import CitizenScientist
 
-# Create your models here.
+
+class Challange(models.Model):
+    name = models.CharField(max_length=250, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    max_points = models.IntegerField(default=10)
+    users = models.ManyToManyField(CitizenScientist)
+
+    def __str__(self) -> str:
+        return f"Challange: {self.name}"
--- iMap/settings.py
+++ iMap/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'observations',
     'users',
+    'challenges'
 ]
 
 MIDDLEWARE = [
